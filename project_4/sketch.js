51aa41ae7e0735a2ba9caeea01753c86947530b4
ygj05001 <114197216+ygj05001@users.noreply.github.com>
2022-11-17 22:23:43 +0900

    ok

--- project_4/sketch.js
+++ project_4/sketch.js
@@ -1,152 +1,132 @@
-const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d");
-const img = document.querySelector("img");
+var Example = Example || {};
 
-let built = false;
-let pixels = [];
-const config = {
-  strength: 2000,
-  easeFactor: 0.1,
-  glow: true,
-  stats: false,
-  pxSize: 15
-};
-const mousePosition = { x: 0, y: 0 };
+Example.bridge = function() {
+    var Engine = Matter.Engine,
+        Render = Matter.Render,
+        Runner = Matter.Runner,
+        Body = Matter.Body,
+        Composites = Matter.Composites,
+        Common = Matter.Common,
+        Constraint = Matter.Constraint,
+        MouseConstraint = Matter.MouseConstraint,
+        Mouse = Matter.Mouse,
+        Composite = Matter.Composite,
+        Bodies = Matter.Bodies;
 
-function resize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  buildPixelData();
-}
+    // create engine
+    var engine = Engine.create(),
+        world = engine.world;
 
-function draw() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  stats.begin();
-  if (!built) {
-    this.buildPixelData();
-  }
+    // create renderer
+    var render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: 800,
+            height: 600,
+            showAngleIndicator: true
+        }
+    });
 
-  for (let i = 0; i < pixels.length; i++) {
-    const pixel = pixels[i];
-    const deltaX = pixel.x - mousePosition.x;
-    const deltaY = pixel.y - mousePosition.y;
-    const angle = Math.atan2(deltaY, deltaX);
-    let distance =
-      config.strength / Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    Render.run(render);
 
-    if (config.glow) {
-      pixel.r += distance;
-      pixel.g += distance;
-      pixel.b += distance;
-      pixel.r += (pixel.or - pixel.r) * config.easeFactor;
-      pixel.g += (pixel.og - pixel.g) * config.easeFactor;
-      pixel.b += (pixel.ob - pixel.b) * config.easeFactor;
-      context.fillStyle = `rgb(${pixel.r}, ${pixel.g}, ${pixel.b})`;
-    } else {
-      context.fillStyle = `rgb(${pixel.or}, ${pixel.og}, ${pixel.ob})`;
-    }
+    // create runner
+    var runner = Runner.create();
+    Runner.run(runner, engine);
 
-    pixel.x += Math.cos(angle) * distance;
-    pixel.y += Math.sin(angle) * distance;
-    pixel.x += (pixel.ox - pixel.x) * config.easeFactor;
-    pixel.y += (pixel.oy - pixel.y) * config.easeFactor;
+    // add bodies
+    var group = Body.nextGroup(true);
 
-    context.beginPath();
-    context.arc(pixel.x, pixel.y, pixel.size / 2, 0, Math.PI * 2);
-    context.fill();
-  }
-  stats.end();
-  window.requestAnimationFrame(draw);
-}
-
-function getPixels() {
-  const data = context.getImageData(0, 0, img.width, img.height);
-  const pxs = [];
-  for (let x = 0; x < data.width; x += config.pxSize) {
-    for (let y = 0; y < data.height; y += config.pxSize) {
-      let index = x * 4 + y * 4 * img.width;
-      let alpha = data.data[index + 3];
-      if (alpha > 0) {
-        pxs.push({
-          x: canvas.width / 2 - img.width / 2 + x,
-          y: canvas.height / 2 - img.height / 2 + y,
-          ox: canvas.width / 2 - img.width / 2 + x,
-          oy: canvas.height / 2 - img.height / 2 + y,
-          size: config.pxSize,
-          oSize: config.pxSize,
-          r: data.data[index],
-          g: data.data[index + 1],
-          b: data.data[index + 2],
-          or: data.data[index],
-          og: data.data[index + 1],
-          ob: data.data[index + 2],
-          rgb:
-            "rgb(" +
-            data.data[index] +
-            "," +
-            data.data[index + 1] +
-            "," +
-            data.data[index + 2] +
-            ")"
+    var bridge = Composites.stack(160, 290, 15, 1, 0, 0, function(x, y) {
+        return Bodies.rectangle(x - 20, y, 53, 20, { 
+            collisionFilter: { group: group },
+            chamfer: 5,
+            density: 0.005,
+            frictionAir: 0.05,
+            render: {
+                fillStyle: '#060a19'
+            }
         });
-      }
-    }
-  }
-  return pxs;
+    });
+    
+    Composites.chain(bridge, 0.3, 0, -0.3, 0, { 
+        stiffness: 1,
+        length: 0,
+        render: {
+            visible: false
+        }
+    });
+    
+    var stack = Composites.stack(250, 50, 6, 3, 0, 0, function(x, y) {
+        return Bodies.rectangle(x, y, 50, 50, Common.random(20, 40));
+    });
+
+    Composite.add(world, [
+        bridge,
+        stack,
+        Bodies.rectangle(30, 490, 220, 380, { 
+            isStatic: true, 
+            chamfer: { radius: 20 }
+        }),
+        Bodies.rectangle(770, 490, 220, 380, { 
+            isStatic: true, 
+            chamfer: { radius: 20 }
+        }),
+        Constraint.create({ 
+            pointA: { x: 140, y: 300 }, 
+            bodyB: bridge.bodies[0], 
+            pointB: { x: -25, y: 0 },
+            length: 2,
+            stiffness: 0.9
+        }),
+        Constraint.create({ 
+            pointA: { x: 660, y: 300 }, 
+            bodyB: bridge.bodies[bridge.bodies.length - 1], 
+            pointB: { x: 25, y: 0 },
+            length: 2,
+            stiffness: 0.9
+        })
+    ]);
+
+    // add mouse control
+    var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+            mouse: mouse,
+            constraint: {
+                stiffness: 0.1,
+                render: {
+                    visible: false
+                }
+            }
+        });
+
+    Composite.add(world, mouseConstraint);
+
+    // keep the mouse in sync with rendering
+    render.mouse = mouse;
+
+    // fit the render viewport to the scene
+    Render.lookAt(render, {
+        min: { x: 0, y: 0 },
+        max: { x: 800, y: 600 }
+    });
+
+    // context for MatterTools.Demo
+    return {
+        engine: engine,
+        runner: runner,
+        render: render,
+        canvas: render.canvas,
+        stop: function() {
+            Matter.Render.stop(render);
+            Matter.Runner.stop(runner);
+        }
+    };
+};
+
+Example.bridge.title = 'Bridge';
+Example.bridge.for = '>=0.14.2';
+
+if (typeof module !== 'undefined') {
+    module.exports = Example.bridge;
 }
-
-function buildPixelData() {
-  if(img.width === 0 || img.height === 0){
-     return;
-  }
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(img, 0, 0);
-  pixels = [];
-  pixels = getPixels();
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  if (pixels.length > 0) {
-    built = true;
-  }
-}
-
-function buildGUI() {
-  const gui = new dat.GUI({ closed: true });
-  gui.closed = true;
-  gui.add(config, "strength", 0, 7000);
-  gui.add(config, "easeFactor", 0.05, 0.2);
-  gui.add(config, "glow");
-  const sizeController = gui.add(config, "pxSize", 8, 80, 1);
-  const statsController = gui.add(config, "stats");
-  sizeController.onFinishChange(() => {
-    buildPixelData();
-  });
-  statsController.onChange(value => {
-    if (value) {
-      document.body.appendChild(stats.dom);
-    } else {
-      stats.dom.parentNode.removeChild(stats.dom);
-    }
-  });
-}
-
-function onMouseMove(event) {
-  mousePosition.x = event.clientX;
-  mousePosition.y = event.clientY;
-}
-
-function touchMoveHandler(event) {
-  mousePosition.x = event.touches[0].clientX;
-  mousePosition.y = event.touches[0].clientY;
-}
-
-const stats = new Stats();
-stats.showPanel(0);
-
-window.addEventListener("DOMContentLoaded", event => {
-  buildGUI();
-  resize();
-  window.addEventListener("mousemove", onMouseMove);
-  window.addEventListener("touchmove", touchMoveHandler);
-  window.addEventListener("resize", resize);
-  window.requestAnimationFrame(draw);
-});
